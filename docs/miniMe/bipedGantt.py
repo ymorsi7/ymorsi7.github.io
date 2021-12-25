317dc336f26271d61adf7ff0216d2ff1c0d503ce
Yusuf Morsi <ymorsi7@gmail.com>
2021-12-24 22:21:22 -0800

    gantt chart update

--- docs/miniMe/bipedGantt.py
+++ docs/miniMe/bipedGantt.py
@@ -11,7 +11,7 @@ df = pd.DataFrame([
     dict(Task="Code for Remote Control of Biped ", Start='2022-3-18', Finish='2022-3-28', Type = 'Testing'),
     dict(Task="Camera Mounting Apparatus CAD Design + Print ", Start='2022-3-29', Finish='2022-4-15', Type = 'Design'),
     dict(Task="Camera Mounting and Testing ", Start='2022-4-16', Finish='2022-4-25', Type = 'Design'),
-    dict(Task="Object Detection with OpenCV ", Start='2022-4-26', Finish='2022-5-20', Type = 'Programming'),
+    dict(Task="Object Detection with OpenCV and PyTorch", Start='2022-4-26', Finish='2022-5-20', Type = 'Programming'),
     dict(Task="Literature Review ", Start='2022-5-20', Finish='2022-6-14', Type = 'Research'),
     dict(Task="Avoiding Detected Objects Code ", Start='2022-6-14', Finish='2022-6-24', Type = 'Programming'),
     dict(Task="Avoiding Detected Objects Testing ", Start='2022-6-14', Finish='2022-7-10', Type = 'Testing'),
