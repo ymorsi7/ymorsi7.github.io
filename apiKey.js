59bed49e24f3e9de9b047ba13cb4dfc0efb2522d
Yusuf Morsi <ymorsi7@gmail.com>
2023-07-14 09:04:45 -0700

    ok

---
+++ apiKey.js
@@ -0,0 +1,3 @@
+const apiKey = 'AIzaSyBRXCN2hnbGO77L_Sb4SzkSIWYvh5rpqPA';
+export default apiKey; // https://maps.googleapis.com/maps/api/js?key=AIzaSyBRXCN2hnbGO77L_Sb4SzkSIWYvh5rpqPA&callback=initMap
+
